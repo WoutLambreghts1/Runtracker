c0228a710216e668a02a86041fb54988887e0725
Wout Lambreghts <wout.lambreghts@student.kdg.be>
2017-02-18 19:54:28 +0100

    oplossing delete competition FK constraints

--- src/main/java/be/kdg/runtracker/backend/services/impl/CompetitionServiceImpl.java
+++ src/main/java/be/kdg/runtracker/backend/services/impl/CompetitionServiceImpl.java
@@ -1,7 +1,6 @@
 package be.kdg.runtracker.backend.services.impl;
 
 import be.kdg.runtracker.backend.dom.competition.Competition;
-import be.kdg.runtracker.backend.dom.competition.Goal;
 import be.kdg.runtracker.backend.dom.profile.User;
 import be.kdg.runtracker.backend.dom.tracking.Tracking;
 import be.kdg.runtracker.backend.persistence.api.CompetitionRepository;
@@ -93,8 +92,7 @@ public class CompetitionServiceImpl implements CompetitionService {
         competition.setTrackings(null);
         this.competitionRepository.save(competition);
 
-        Goal goal = competition.getGoal();
-        if (goal != null) this.goalRepository.delete(goal.getGoalId());
+        competition.setGoal(null);
 
         this.competitionRepository.delete(competition.getCompetitionId());
     }
